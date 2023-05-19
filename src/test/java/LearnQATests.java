2cb6453c60532570e7234b26cd85303ce1b3653a
pomah <pomah14021997@mail.ru>
2023-05-19 12:08:58 +0300

    Ex4: GET-запрос

--- src/test/java/LearnQATests.java
+++ src/test/java/LearnQATests.java
@@ -1,3 +1,5 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class LearnQATests {
@@ -5,6 +7,14 @@ public class LearnQATests {
     @Test
     public void hello(){
         System.out.println("Hello from Roman Miranyuk");
+    }
+
+    @Test
+    public void responsePrint(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
     }
 
 }
