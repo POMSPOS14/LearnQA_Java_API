864a55699995ce4b738d898e4a1e98acde311cd6
pomah <pomah14021997@mail.ru>
2023-06-09 13:47:08 +0300

    Ex11: Тест запроса на метод cookie

--- src/test/java/LearnQATestsPartThree.java
+++ src/test/java/LearnQATestsPartThree.java
@@ -1,7 +1,11 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -12,5 +16,13 @@ public class LearnQATestsPartThree {
     @ValueSource(strings = {"lessfifteen", "more than fifteen"})
     public void shortPhrase(@NotNull String string) {
         assertTrue(string.length() > 15, "String have less than fifteen characters");
+    }
+
+    @Test
+    public void responsePrint() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+        assertEquals(response.getCookie("HomeWork"),"hw_value", "Cookie not valid");
     }
 }
