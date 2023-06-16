94a377bc2caa583cd77be94de16090e3d00ffefe
pomah <pomah14021997@mail.ru>
2023-06-16 18:04:59 +0300

    Ex15: Тесты на метод user

--- src/test/java/lib/DataGeneration.java
+++ src/test/java/lib/DataGeneration.java
@@ -10,6 +10,11 @@ public class DataGeneration {
         return "learnqa" + timestamp + "@example.com";
     }
 
+    public static String getRandomEmailWithDefect() {
+        String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new java.util.Date());
+        return "learnqa" + timestamp + "example.com";
+    }
+
     public static Map<String, String> getRegistrationData() {
         Map<String, String> data = new HashMap<>();
         data.put("email", DataGeneration.getRandomEmail());
@@ -20,18 +25,53 @@ public class DataGeneration {
         return data;
     }
 
-    public static Map<String,String> getRegistrationData(Map<String,String> nonDefaultValues) {
-        Map<String,String> defaultValues = DataGeneration.getRegistrationData();
-        Map<String,String> userData = new HashMap<>();
-        String[] keys = {"email","password","username","firstName","lastName"};
-        for (String key:keys){
-            if(nonDefaultValues.containsKey(key)) {
+    public static Map<String, String> getRegistrationDataWithOutParam(String paramForDelete) {
+        Map<String, String> data = new HashMap<>();
+        data.put("email", DataGeneration.getRandomEmail());
+        data.put("password", "123");
+        data.put("username", "lernqa");
+        data.put("firstName", "lernqa");
+        data.put("lastName", "lernqa");
+        data.remove(paramForDelete);
+        return data;
+    }
+
+    public static Map<String, String> getRegistrationDataWithShortName() {
+        Map<String, String> data = new HashMap<>();
+        data.put("email", DataGeneration.getRandomEmail());
+        data.put("password", "123");
+        data.put("username", "l");
+        data.put("firstName", "lernqa");
+        data.put("lastName", "lernqa");
+        return data;
+    }
+
+    public static Map<String, String> getRegistrationDataWithLongName() {
+        String name = "";
+        for (int i = 0; i < 260; i++) {
+            name = name + "l";
+        }
+        Map<String, String> data = new HashMap<>();
+        data.put("email", DataGeneration.getRandomEmail());
+        data.put("password", "123");
+        data.put("username", name);
+        data.put("firstName", "lernqa");
+        data.put("lastName", "lernqa");
+        return data;
+    }
+
+
+    public static Map<String, String> getRegistrationData(Map<String, String> nonDefaultValues) {
+        Map<String, String> defaultValues = DataGeneration.getRegistrationData();
+        Map<String, String> userData = new HashMap<>();
+        String[] keys = {"email", "password", "username", "firstName", "lastName"};
+        for (String key : keys) {
+            if (nonDefaultValues.containsKey(key)) {
                 userData.put(key, nonDefaultValues.get(key));
             } else {
-                userData.put(key,defaultValues.get(key));
+                userData.put(key, defaultValues.get(key));
             }
         }
         return userData;
-
     }
 }
--- src/test/java/tests/UserRegisterTest.java
+++ src/test/java/tests/UserRegisterTest.java
@@ -8,6 +8,8 @@ import lib.BaseTestCase;
 import lib.DataGeneration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,23 +25,78 @@ public class UserRegisterTest extends BaseTestCase {
         userData.put("email", email);
         userData = DataGeneration.getRegistrationData(userData);
 
-        Response responseCreatAuth = apiCoreRequests
+        Response responseCreateAuth = apiCoreRequests
                 .makePostRequest("https://playground.learnqa.ru/api/user/", userData);
-        Assertions.asserResponseCodeEquals(responseCreatAuth, 400);
-        Assertions.asserResponseTextEquals(responseCreatAuth, "Users with email '" + email + "' already exists");
+        Assertions.asserResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.asserResponseTextEquals(responseCreateAuth, "Users with email '" + email + "' already exists");
     }
 
     @Test
     @Description("Successfully registration user with new data")
     @DisplayName("Create user successfully")
-    public void testCresteUserSuccessfully() {
+    public void testCreateUserSuccessfully() {
 
         Map<String,String> userData = DataGeneration.getRegistrationData();
 
-        Response responseCreatAuth = apiCoreRequests
+        Response responseCreateAuth = apiCoreRequests
                 .makePostRequest("https://playground.learnqa.ru/api/user/", userData);
-        Assertions.asserResponseCodeEquals(responseCreatAuth, 200);
-        Assertions.assertJsonHasField(responseCreatAuth, "id");
+        Assertions.asserResponseCodeEquals(responseCreateAuth, 200);
+        Assertions.assertJsonHasField(responseCreateAuth, "id");
+    }
+
+    @Test
+    @Description("Negative registration user with email without @")
+    @DisplayName("Registration with defect email")
+    public void testCreateUserWithDefectEmail() {
+        String email = DataGeneration.getRandomEmailWithDefect();
+        Map<String, String> userData = new HashMap<>();
+        userData.put("email", email);
+        userData = DataGeneration.getRegistrationData(userData);
+
+        Response responseCreateAuth = apiCoreRequests
+                .makePostRequest("https://playground.learnqa.ru/api/user/", userData);
+        Assertions.asserResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.asserResponseTextEquals(responseCreateAuth, "Invalid email format");
+    }
+
+    @ParameterizedTest
+    @Description("Negative to registration user without one of the parameters")
+    @DisplayName("Creating a user without one of the parameters")
+    @ValueSource(strings = { "email", "password", "username", "firstName", "lastName" })
+    public void testCreateUserWithoutAnyParam(String param) {
+        Map<String, String> userData = DataGeneration.getRegistrationDataWithOutParam(param);
+
+        Response responseCreateAuth = apiCoreRequests
+                .makePostRequest("https://playground.learnqa.ru/api/user/", userData);
+
+        Assertions.asserResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.asserResponseTextEquals(responseCreateAuth, "The following required params are missed: " + param);
+    }
+
+    @Test
+    @Description("Negative to registration user with short name")
+    @DisplayName("Creating a user with short name")
+    public void testCreateUserWithShortName() {
+        Map<String, String> userData = DataGeneration.getRegistrationDataWithShortName();
+
+        Response responseCreateAuth = apiCoreRequests
+                .makePostRequest("https://playground.learnqa.ru/api/user/", userData);
+
+        Assertions.asserResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.asserResponseTextEquals(responseCreateAuth, "The value of 'username' field is too short");
+    }
+
+    @Test
+    @Description("Negative to registration user with long name")
+    @DisplayName("Creating a user with long name")
+    public void testCreateUserWithLongName() {
+        Map<String, String> userData = DataGeneration.getRegistrationDataWithLongName();
+
+        Response responseCreateAuth = apiCoreRequests
+                .makePostRequest("https://playground.learnqa.ru/api/user/", userData);
+
+        Assertions.asserResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.asserResponseTextEquals(responseCreateAuth, "The value of 'username' field is too long");
     }
 
 }
